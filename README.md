0fe037e8809c0e6f20ff0ea6c6000cbb1a7b1678
Simen A. W. Olsen <so@bjerk.io>
2023-03-27 10:53:46 +0200

    style: format with prettier

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Branches: Procore – Abax Backoffice
 
-This repository contains the Sanity studio that runs the Procore – ABAX integration.
+This repository contains the Sanity studio that runs the Procore – ABAX
+integration.
--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: 'j1f6z3dh',
-    dataset: 'production'
-  }
-})
+    dataset: 'production',
+  },
+});
